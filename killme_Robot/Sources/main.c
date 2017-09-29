a4a4a12e22cb5a349956e43d39a8eeb237ec7aed
Florian Lussi <florian.lussi.01@stud.hslu.ch>
2017-09-29 12:57:50 +0200

    LED Blink

--- killme_Robot/Sources/main.c
+++ killme_Robot/Sources/main.c
@@ -30,6 +30,10 @@
 /* Including needed modules to compile this module/procedure */
 #include "Cpu.h"
 #include "Events.h"
+#include "Bit1.h"
+#include "BitIoLdd1.h"
+#include "WAIT1.h"
+#include "MCUC1.h"
 /* Including shared modules, which are used for whole project */
 #include "PE_Types.h"
 #include "PE_Error.h"
@@ -48,6 +52,14 @@ int main(void)
   /*** End of Processor Expert internal initialization.                    ***/
 
   /* Write your code here */
+  while(1)
+  {
+	  WAIT1_Waitms(1000);
+	 Bit1_SetVal();
+	 WAIT1_Waitms(1000);
+	 Bit1_ClrVal();
+  }
+
   /* For example: for(;;) { } */
 
   /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
