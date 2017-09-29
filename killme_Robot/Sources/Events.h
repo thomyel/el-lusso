1ea5efe235ac076ba217e81b169e6cf40e3ebca7
thomyel <thomyel@users.noreply.github.com>
2017-09-29 14:59:04 +0200

    Merge branch 'master' of https://github.com/thomyel/el-lusso

--- killme_Robot/Sources/Events.h
+++ killme_Robot/Sources/Events.h
@@ -33,6 +33,10 @@
 #include "PE_Error.h"
 #include "PE_Const.h"
 #include "IO_Map.h"
+#include "Bit1.h"
+#include "BitIoLdd1.h"
+#include "WAIT1.h"
+#include "MCUC1.h"
 
 #ifdef __cplusplus
 extern "C" {
