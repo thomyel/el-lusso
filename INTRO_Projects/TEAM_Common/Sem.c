f96ff450b5807e4f81d6c4b58d9f55793c3833aa
thomyel <thomyel@users.noreply.github.com>
2017-11-17 10:53:15 +0100

    Implemented Semaphore Lab

--- INTRO_Projects/TEAM_Common/Sem.c
+++ INTRO_Projects/TEAM_Common/Sem.c
@@ -24,10 +24,25 @@ static xSemaphoreHandle sem = NULL;
 
 static void vSlaveTask(void *pvParameters) {
   /*! \todo Implement functionality */
+
+	// Slave Task with the required parameters (Take the Semaphore, toggle LED and wait for 1.5s)
+	TickType_t xLastWakeTime = xTaskGetTickCount();
+	for(;;){
+		xSemaphoreTake(sem, 0);
+		LEDPin2_NegVal();
+		vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(1500));
+	}
 }
 
 static void vMasterTask(void *pvParameters) {
   /*! \todo send semaphore from master task to slave task */
+
+	// Master Task with the required parameters (Give the Semaphore and wait for 1.5s)
+	TickType_t xLastWakeTime = xTaskGetTickCount();
+	for(;;){
+		xSemaphoreGive(sem);
+		vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(1500));
+	}
 }
 
 void SEM_Deinit(void) {
@@ -35,5 +50,21 @@ void SEM_Deinit(void) {
 
 /*! \brief Initializes module */
 void SEM_Init(void) {
+
+	  // Task creation for SlaveTask - it takes the semaphore for LED Blink
+	  if (xTaskCreate(vSlaveTask, "Slave Task", configMINIMAL_STACK_SIZE+100, NULL, tskIDLE_PRIORITY, NULL)!=pdPASS) {
+		  for(;;) {} /* error? */
+	  }//if (xTaskCreate)
+
+	  //  Task creation for MasterSlave - it gives the semaphore to other Task --> SlaveTask
+	  if (xTaskCreate(vMasterTask, "Master Task", configMINIMAL_STACK_SIZE+100, NULL, tskIDLE_PRIORITY, NULL)!=pdPASS) {
+		  for(;;) {} /* error? */
+	  }//if (xTaskCreate)
+
+	  // Create a Binary Semaphore
+	  sem = xSemaphoreCreateBinary();
+	  if(sem == NULL){	// Check if the Sempahore exist, when no, then goes to endless-loop
+		  for(;;) {} /* error handling */
+	  }
 }
 #endif /* PL_CONFIG_HAS_SEMAPHORE */
--- INTRO_Projects/TEAM_Robot/Sources/Platform_Local.h
+++ INTRO_Projects/TEAM_Robot/Sources/Platform_Local.h
@@ -41,7 +41,7 @@
 #define PL_LOCAL_CONFIG_HAS_USB_CDC_DISABLED              /* disable USB CDC */
 //#define PL_LOCAL_CONFIG_HAS_SHELL_QUEUE_DISABLED          /* disable shell queue */
 #define PL_LOCAL_CONFIG_HAS_SQUEUE_SINGLE_CHAR_DISABLED   /* disable single character support in shell queue */
-#define PL_LOCAL_CONFIG_HAS_SEMAPHORE_DISABLED            /* disable semaphore test module */
+//#define PL_LOCAL_CONFIG_HAS_SEMAPHORE_DISABLED            /* disable semaphore test module */
 #define PL_LOCAL_CONFIG_HAS_CONFIG_NVM_DISABLED           /* disable NVM storage */
 
 /* remote controller hardware functionality */
